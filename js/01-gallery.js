4032804327cc75ea7eb67bca3506372da084c96f
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-04-03 17:07:00 +0200

    Basic gallery built

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+galleryItems.innerHTML = '';
+for (const galleryItem of galleryItems) {
+
+    const newItem = `<a class="gallery__link" href="${galleryItem.original}">
+
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+      />
+      </a>`
+    
+    gallery.innerHTML += newItem;
+};
+
 console.log(galleryItems);
