b2a30015483be564f9c22a44524042f3774bc8a8
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-04-04 15:12:23 +0200

    Basic gallery built

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+galleryItems.innerHTML = '';
+for (const galleryItem of galleryItems) {
+
+    const newItem = `
+    
+    <li class="gallery__item"><a class="gallery__link" href="${galleryItem.original}">
+
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+      />
+      </a>
+      </li>`
+    
+    gallery.innerHTML += newItem;
+};
+
 console.log(galleryItems);
